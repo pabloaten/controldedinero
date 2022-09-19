19852b4203b8998c32f48b385068fe8887dcbc3f
pabloaten <pabloatenciano@gmail.com>
2022-09-19 20:57:00 +0200

    A

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [item2, setItem2] = useState([]);
   const [objetos, setObjetos] = useState([]);
   const [total, setTotal] = useState(0);
+  const [fecha, setFecha] = useState("");
 
   const current = new Date();
   const date = `${current.getFullYear()}/${current.getMonth() + 1
@@ -39,10 +40,21 @@ function App() {
   };
 
   const handleSave = () => {
-  
-    createItem(item, item2, date);
-
-    alertify.success("Se ha creado correctamente");
+    
+    
+    if(item.length == 0 || item2.length == 0){
+      alertify
+  .alert("Rellena los campos.", function(){
+    
+  });
+      alertify.error("Rellena los campos");
+    }else{
+      createItem(item, item2, date);
+      alertify.success("Se ha creado correctamente");
+      setItem("");
+      setItem2("");
+    }
+    
 
     obtenerDatos();
   };
@@ -54,6 +66,19 @@ function App() {
       let b = new Date(d.data().obj3);
      console.log(a);
         return a-b;
+      
+    }));
+    console.log(objetos[0].data())
+
+  }
+  const handleOrdenarFechaInverso = async () => {
+    const p = await getItems();
+    
+    setObjetos(p.docs.sort(function (c, d) {
+      let a = new Date(c.data().obj3);
+      let b = new Date(d.data().obj3);
+     console.log(a);
+        return b-a;
       
     }));
     console.log(objetos[0].data())
@@ -109,8 +134,26 @@ function App() {
     alertify.error('Cancel');
   });
     
-    obtenerDatos();
+    
   };
+  const handleBuscarFecha = async () => {
+    /* console.log(setFecha(nombre)); */
+    console.log(fecha.toString())
+    const p = await getItems();
+    let fecha2 = new Date(fecha);
+    const fecha3 = `${fecha2.getFullYear()}/${fecha2.getMonth() + 1
+    }/${fecha2.getDate()}`;
+    console.log(fecha3+" fecha")
+    setObjetos(p.docs.filter(a => a.data().obj3 == fecha3));
+    let valor = 0;
+    for (const key of p.docs.filter(a => a.data().obj3 == fecha3)) {
+      console.log(key.data().obj2)
+      
+      valor+=parseFloat(key.data().obj2);;
+    }
+    setTotal(valor);
+    
+  }
   return (
     <div className="fondo uk-background-width-1-1 uk-background-norepeat uk-background-top-center uk-flex uk-flex-column  uk-flex-middle">
     <div class="grid"></div>
@@ -149,7 +192,18 @@ function App() {
         <button class="uk-button naranja uk-margin" onClick={handleOrdenarFecha}>
           Fecha
         </button>
-        
+        <button class="uk-button naranja uk-margin" onClick={handleOrdenarFechaInverso}>
+          Fecha
+        </button>
+        <button class="uk-button naranja " onClick={obtenerDatos}>
+          Reset
+        </button>
+        <button href="#toggle-animation" class="uk-button naranja uk-button-default" type="button" uk-toggle="target: #toggle-animation; animation: uk-animation-fade">Fecha especifica</button>
+<div className="uk-margin" id="toggle-animation"> <input className="uk-input"  value={fecha} onChange={(e) =>setFecha(e.target.value)} type="date" />
+        <button class="uk-button naranja uk-margin" onClick={handleBuscarFecha}>
+          Buscar
+        </button></div>
+       
         <table className="uk-table">
           <thead>
             <tr>
