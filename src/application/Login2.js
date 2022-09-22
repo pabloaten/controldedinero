90a93cadc9e4deb1224044608aba8ada2aff2ae2
pabloaten <pabloatenciano@gmail.com>
2022-09-22 19:23:08 +0200

    A

---
+++ src/application/Login2.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const Login2 = ({user,setUser}) => {
+    
+const provider = new GoogleAuthProvider();
+const logearse = () =>{
+    const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user)
+    setUser(user.email)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+}
+
+  return (
+    <div> 
+        <button onClick={logearse} class="btn-glitch-fill ">
+    <span class="text">// Acceder</span>
+    <span class="text-decoration">_</span>
+    <span class="decoration">&rArr;</span>
+  </button></div>
+  )
+}
+
+export default Login2
